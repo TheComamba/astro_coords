beefa2955a7e6d9d1265c3b9ca3c9bc2f7ada9ea
TheComamba <thecomamba@jojoheinze.de>
2024-07-06 11:11:29 +0200

    Moving stuff.

---
+++ src/mod.rs
@@ -0,0 +1,9 @@
+pub mod cartesian;
+pub mod declination;
+pub mod direction;
+pub mod earth_equatorial;
+pub mod ecliptic;
+pub mod equatorial;
+pub mod right_ascension;
+pub mod spherical;
+pub mod transformations;
