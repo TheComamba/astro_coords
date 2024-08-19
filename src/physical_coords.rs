f4c9f1e392faa248c124369efc1bd7acd504f9c3
TheComamba <thecomamba@jojoheinze.de>
2024-08-19 09:12:09 +0200

    Documentation for new functions.

--- src/physical_coords.rs
+++ src/physical_coords.rs
@@ -15,7 +15,7 @@ impl<T> PhysicalCoords<T>
 where
     T: Mathematical + ActiveRotation<T>,
 {
-    /// Create a new PhysicalCartesian.
+    /// Create a new PhysicalCartesian object.
     pub fn new(inner: T, reference_frame: ReferenceFrame) -> Self {
         Self {
             reference_frame,
@@ -28,8 +28,28 @@ impl<T> Physical for PhysicalCoords<T>
 where
     T: Mathematical + ActiveRotation<T>,
 {
+    /// Returns the frame of reference that the mathematical coordinates are defined in.
     fn reference_frame(&self) -> ReferenceFrame {
         self.reference_frame
+    }
+
+    /// Changes the frame of reference, transforming the mathematical coordinates.
+    ///
+    /// The transformation is defined by a new z-axis, and an angle-parameter W that specifies the orientation of the new x-axis.
+    /// Based on Fig. 1 of [the IAU report](https://astropedia.astrogeology.usgs.gov/download/Docs/WGCCRE/WGCCRE2015reprint.pdf), the algorithm is as follows:
+    /// - Express the new z-axis in the old reference frame.
+    /// - Convert that new-z-axis to spherical coordinates to determine the longitude (alpha) and latitude (delta) of the new z-axis.
+    /// - Rotate around the old z-axis by 90°-a. The current x-axis now points to the intermediate direction Q.
+    /// - Rotate around Q by 90°-d. The current z-axis now points to the new z-axis.
+    /// - Rotate around the new z-axis by W. The current x-axis now points to the new x-axis.
+    fn change_reference_frame(&mut self, new_frame: ReferenceFrame) {
+        todo!();
+        self.reference_frame = new_frame;
+    }
+
+    /// Overwrites the frame of reference without transforming the mathematical coordinates.
+    fn overwrite_reference_frame(&mut self, new_frame: ReferenceFrame) {
+        self.reference_frame = new_frame;
     }
 }
 
--- src/traits/physical.rs
+++ src/traits/physical.rs
@@ -3,4 +3,10 @@ use crate::reference_frame::ReferenceFrame;
 pub trait Physical {
     /// Returns the frame of reference that the mathematical coordinates are defined in.
     fn reference_frame(&self) -> ReferenceFrame;
+
+    /// Changes the frame of reference, transforming the mathematical coordinates.
+    fn change_reference_frame(&mut self, new_frame: ReferenceFrame);
+
+    /// Overwrites the frame of reference without transforming the mathematical coordinates.
+    fn overwrite_reference_frame(&mut self, new_frame: ReferenceFrame);
 }
