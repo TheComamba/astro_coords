79352d890fb9b31b068646a070a05bcc768e432e
TheComamba <thecomamba@jojoheinze.de>
2024-08-19 08:36:59 +0200

    Documenting reference frames, making Equatorial Earth-specific, adding galactic coordinates.

--- src/reference_frame.rs
+++ src/reference_frame.rs
@@ -7,8 +7,24 @@ use simple_si_units::geometry::Angle;
 /// This enum needs to be provided  to eliminate ambiguity when converting between different coordinate systems.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum ReferenceFrame {
-    Equatorial(CelestialBody),
+    /// The Earth-Equatorial reference frame.
+    ///
+    /// Equatorial coordinates are oriented such that the z-axis points along the Earth's axis of rotation or north pole. The remaining orientation ambiguity is resolved by any of the four following facts:
+    /// - At the vernal equinox, the x-axis points directly towards the sun.
+    /// - At the summer solstice, when the sun appears high in the sky on the northern hemisphere, its y-projection and z-projection are both positive.
+    /// - At the autumn equinox, the x-axis points directly away from the sun.
+    /// - At the winter solstice, when the sun appears high in the sky on the southern hemisphere, its y-projection and z-projection are both negative.
+    ///
+    /// Note that the orientation of the coordinate system does not change with time, even though the Earth is rotating.
+    Equatorial,
+    /// The Earth-Ecliptic reference frame.
+    ///
+    /// Ecliptic coordinates are oriented such that the z-axis points along the normal of the plane of the Earth's orbit around the sun. The x-axis points towards the vernal equinox (the same as for Equatorial coordinates), and the y-axis completes the right-handed coordinate system.
     Ecliptic,
+    /// The Galactic reference frame.
+    ///
+    /// Galactic coordinates are oriented such that the z-axis points along the normal of the plane of the Milky Way galaxy. The x-axis points towards the galactic center, and the y-axis completes the right-handed coordinate system.
+    Galactic,
 }
 
 /// This enum contains the celestial bodies for which the equatorial reference frame can be defined.
