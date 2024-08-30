5c3fa1650cc992f83ab9d645411b68e9867424bb
TheComamba <thecomamba@jojoheinze.de>
2024-08-30 21:20:00 +0200

    Found references for earth rotational parameters. [no ci]

--- src/reference_frame/celestial_body.rs
+++ src/reference_frame/celestial_body.rs
@@ -1,7 +1,7 @@
 use simple_si_units::{base::Time, geometry::Angle};
 use std::fmt::Display;
 
-use crate::spherical::Spherical;
+use crate::{angle_helper::FULL_CIRC, spherical::Spherical};
 
 /// This enum contains the celestial bodies for which the equatorial reference frame can be defined.
 ///
@@ -85,9 +85,38 @@ impl CelestialBody {
 
     /// Returns the prime meridian offset of the reference frame.
     ///
-    /// The prime meridian offset is the longitudal angle offset of the x-axis of the reference frame, relative to the point Q that the equatorial x-axis points at after rotating to the new z-axis. It is denoted W in [Fig. 1 of the IAU Report](https://astropedia.astrogeology.usgs.gov/download/Docs/WGCCRE/WGCCRE2015reprint.pdf).
+    /// The prime meridian offset is the longitudal angle offset of the x-axis of the reference frame, relative to the point Q that the equatorial x-axis points at after rotating to the new z-axis. It is the current longitudial offset from the direction of earth's vernal equinox, or the x-direction in equatorial coordinates. It is denoted W in [Fig. 1 of the IAU Report](https://astropedia.astrogeology.usgs.gov/download/Docs/WGCCRE/WGCCRE2015reprint.pdf).
+    /// The data here is taken from the [Report of the IAU Working Group on Cartographic Coordinates and Rotational Elements: 2015](https://astropedia.astrogeology.usgs.gov/download/Docs/WGCCRE/WGCCRE2015reprint.pdf), except for that of earth, which is taken from Eq. 5.14 of [chapter 5 of technical note 36 of the international earth rotation and reference systems service](https://www.iers.org/SharedDocs/Publikationen/EN/IERS/Publications/tn/TechnNote36/tn36_043.pdf?__blob=publicationFile&v=1).
     pub fn prime_meridian_offset(&self, time_since_epoch: Time<f64>) -> Angle<f64> {
         match self {
+            CelestialBody::Sun => {
+                Angle::from_deg(84.176) + Angle::from_deg(14.1844000) * time_since_epoch.to_days()
+            }
+            CelestialBody::Mercury => {
+                Angle::from_deg(329.5988) + Angle::from_deg(6.1385108) * time_since_epoch.to_days()
+            }
+            CelestialBody::Venus => {
+                Angle::from_deg(160.20) - Angle::from_deg(1.4813688) * time_since_epoch.to_days()
+            }
+            CelestialBody::Earth => {
+                FULL_CIRC * (0.7790572732640 + 1.00273781191135448 * time_since_epoch.to_days())
+            }
+            CelestialBody::Mars => {
+                Angle::from_deg(176.049863)
+                    + Angle::from_deg(350.891982443297) * time_since_epoch.to_days()
+            }
+            CelestialBody::Jupiter => {
+                Angle::from_deg(284.95) + Angle::from_deg(870.5360000) * time_since_epoch.to_days()
+            }
+            CelestialBody::Saturn => {
+                Angle::from_deg(38.90) + Angle::from_deg(810.7939024) * time_since_epoch.to_days()
+            }
+            CelestialBody::Uranus => {
+                Angle::from_deg(203.81) - Angle::from_deg(501.1600928) * time_since_epoch.to_days()
+            }
+            CelestialBody::Neptune => {
+                Angle::from_deg(249.978) + Angle::from_deg(541.1397757) * time_since_epoch.to_days()
+            }
             _ => todo!(),
         }
     }
